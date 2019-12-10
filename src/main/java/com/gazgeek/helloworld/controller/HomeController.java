a1634dd8f4c9fb25db96b92f9a0d788b36cd4f4d
765612 <765612@PCIN456823.cts.com>
2019-12-10 17:00:26 +0530

    Nothing

--- src/main/java/com/gazgeek/helloworld/controller/HomeController.java
+++ src/main/java/com/gazgeek/helloworld/controller/HomeController.java
@@ -3,11 +3,16 @@ package com.gazgeek.helloworld.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.boot.config.user;
+
 @RestController
 public class HomeController {
 
     @RequestMapping("/")
     String home() {
+    
+    	System.out.println(Harsha);
+    	
         return "Hello from GazGeek!";
     }
 
